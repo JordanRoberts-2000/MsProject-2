8b3a439d0ceafad028d795b746c84e9c66adf3a5
JordanRoberts-2000 <jordanroberts654@outlook.com>
2021-06-24 05:27:47 +0000

    Feature Settings selection are functional

--- js/script.js
+++ js/script.js
@@ -8,3 +8,17 @@ $(".navigation").on("mouseleave", function() {
     $(".settings").css("font-size", "120%");
     $(".settings").css("color", "darkgreen");
   });
+  $(".on").on("click", function() {
+    localStorage.setItem("on", "rgba(0, 0, 0, 0.349)");
+    $("body").css("background-color", (localStorage.getItem("on")));
+  });
+  $(".off").on("click", function() {
+    localStorage.setItem("on", "rgb(238, 236, 236)");
+    $("body").css("background-color", (localStorage.getItem("on")));
+  });
+  $(".fontSize").on("input change", function() {
+    localStorage.setItem("fontSize",$(this).val());
+    console.log($(this).val());
+    $("body").css("font-size", ($(this).val())/10 + "em");
+    console.log(localStorage.getItem("fontSize"));
+  });
