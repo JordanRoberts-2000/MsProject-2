79a926fa6356195219e52a880772c6ffa91c32f3
JordanRoberts-2000 <jordanroberts654@outlook.com>
2021-06-24 06:22:02 +0000

    added api function for google maps

--- js/script.js
+++ js/script.js
@@ -37,3 +37,17 @@ $(".navigation").on("mouseleave", function() {
     $("body").css("font-size", ($(this).val())/10 + "em");
     console.log(localStorage.getItem("fontSize"));
   });
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.036 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
